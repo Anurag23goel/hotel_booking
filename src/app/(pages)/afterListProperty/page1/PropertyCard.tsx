82f8a51f8dd6ec59ae98f9bcd0db5d149e7c4c86
KaranNegi0102 <karannegi0102@gmail.com>
2025-03-20 21:59:46 +0530

    changes

--- src/app/(pages)/afterListProperty/page1/PropertyCard.tsx
+++ src/app/(pages)/afterListProperty/page1/PropertyCard.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from 'next/link';
 import { useRouter } from 'next/navigation'
 
 export function PropertyCard({
@@ -11,22 +12,23 @@ export function PropertyCard({
   description: string,
 }) {
   const router = useRouter();
-  const handleClick = () => {
-    switch (title) {
-      case 'Apartment':
-        router.push('/afterListProperty/AppartmentPage');
-        break;
-      case 'Homes':
-        router.push('/afterListProperty/HomesPage');
-        break;
-      case 'Hotel, B&Bs & More':
-        router.push('/afterListProperty/HotelPage');
-        break;
-      case 'Alternative Places':
-        router.push('/afterListProperty/AlternativePage');
-        break;
-    }
-  };
+
+  // const handleClick = () => {
+  //   switch (title) {
+  //     case 'Apartment':
+  //       router.push('/afterListProperty/AppartmentPage');
+  //       break;
+  //     case 'Homes':
+  //       router.push('/afterListProperty/HomesPage');
+  //       break;
+  //     case 'Hotel, B&Bs & More':
+  //       router.push('/afterListProperty/HotelPage');
+  //       break;
+  //     case 'Alternative Places':
+  //       router.push('/afterListProperty/AlternativePage');
+  //       break;
+  //   }
+  // };
 
   return (
     <div className="p-8 rounded-lg shadow-md flex flex-col items-center text-center h-full justify-between">
@@ -37,11 +39,12 @@ export function PropertyCard({
         <h2 className="text-sm font-bold mb-3">{title}</h2>
         <p className="text-gray-600 text-sm mb-6">{description}</p>
       </div>
-      <button
-        onClick={handleClick}
+      <Link
+        href={`/afterListProperty/${title.toLowerCase()}`}
         className="bg-[#274905] text-white px-6 py-2 rounded-md hover:bg-[#83a75f] transition-colors w-full mt-auto">
         List your property
-      </button>
+      </Link>
+
     </div>
   );
 }
